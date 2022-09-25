e8e5947c52c092b249c63a9ce10d711cd49ef552
SajanArora <sajanarora@zomentum.com>
2022-09-25 12:43:56 +0530

    feat: init commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import "tailwindcss/tailwind.css";
 import "../../styles/globals.css";
 
-axios.defaults.baseURL = `${process.env.NEXT_API_BASE_ENDPOINT}`
+axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_API_BASE_ENDPOINT}`
 
 function MyApp({ Component, pageProps }) {
   return <div className="h-screen font-serif bg-gray-800 text-white grid place-items-center">
